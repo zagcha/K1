8b615bbb0be1a3f96974f9ce416d1d59c942b593
zagcha <64097681+zagcha@users.noreply.github.com>
2021-03-17 21:11:47 +0300

    Create Kaz.js

---
+++ Kaz.js
@@ -0,0 +1,40 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تصميم وبرمجه: <a href='https://www.dookmobile.com/'>دوك موبايل</a></div>").insertAfter($(".loginstat"));  },1000);
+
+
+sendpic_=sendpic;
+reqlikes=10;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على '+reqlikes+' لايكات'); return;}else{sendpic_();}}
+
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<img src="sico/1614272759844.png" class="fr" style="width:100%;margin-: 2px;">
+<a id="kaz3" href=" href="
+<a id="kaz3" href="
+
+`).insertBefore('.nav-tabs');
+
+
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( حياكم الله في شات العراق للجوال ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+
+
+$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span>لوحة تحكم شات العراق ")
+
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> إعلانات شات العراق للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
+
+$("[data-target=#wall]").attr('title','حآئط شات العراق : متنفس الآقلأم والقلوب')
+
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 15px!important;text-align: center!important;width: 101%;color: #;white-space: nowrap;border-radius: 14px 14px 14px 14px;}
+.border {shadow:1px 1px 1px 1px ridge rgb(137, 137, 137); box-shadow:1px 1px 1px 1px rgb(137, 137, 137) }
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);border-radius: 5px 5px 5px 5px;;}
+.border.corner, .border.corner {box-shadow: inset 0 0px 0px rgb(137, 137, 137), 0 0 4px rgb(137, 137, 137);border: 2px solid #ffffff;as:13px;}
+
+</style>`).insertBefore('body')
+
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> يا هلا ومرحبا بيكم في شات العراق للجوال ، آملين لكم قضاء أجمل الأوقات معنا      </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
